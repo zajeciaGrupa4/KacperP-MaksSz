ad5735db65b7a304e4f6ca7a39d18eab600f7db2
Maks <maksymilian.szwabowicz@gmail.com>
2026-04-15 11:56:57 +0200

    poprawiony szyfr cezara

--- cezar.py
+++ cezar.py
@@ -1,8 +1,13 @@
 def szyfr_cezara(tekst, klucz):
     wynik = ""
     for litera in tekst:
-        kod = ord(litera) + klucz
-        wynik += chr(kod)
+        if litera.isalpha() and 'a' <= litera.lower() <= 'z':
+            litera = ord(litera.lower()) + klucz
+        if litera > ord('z'):
+            litera -= 26
+        elif litera < ord('a'):
+            litera += 26
+        wynik += chr(litera)
     return wynik
 n = "alamakota"
 k = 3
