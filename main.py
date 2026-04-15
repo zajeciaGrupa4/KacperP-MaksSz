2b8af8951a35490e903aeaef96e12d892da0fdad
Maks <maksymilian.szwabowicz@gmail.com>
2026-04-15 12:11:53 +0200

    szyfr viegenera-deszyfrowanie i test

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print("Działa!")
+
+from viegenr import szyfruj_vigenere, deszyfruj_vigenere
+
+oryginalny = "ALAMAKOTA"
+klucz = "KOT"
+
+zaszyfrowany = szyfruj_vigenere(oryginalny, klucz)
+odszyfrowany = deszyfruj_vigenere(zaszyfrowany, klucz)
+
+print(f"Oryginał: {oryginalny}")
+print(f"Szyfr:    {zaszyfrowany}")
+print(f"Powrót:   {odszyfrowany}")
+
+if oryginalny == odszyfrowany:
+    print("\n✅ Wszystko działa poprawnie!")
